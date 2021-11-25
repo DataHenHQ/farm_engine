6b6dcaa2187e0059ad2d959ab6cb4f7b7cc41685
ER <colorfulsing@gmail.com>
2021-11-24 19:11:05 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+//! # MatchQA
+//! 
+//! `matchqa` is an interactive compare tool used to compare
+//! and match items from a CSV file by creating a web application
+//! over localhost for the user to easilly compare items.
+
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
@@ -12,6 +18,11 @@ use utils::*;
 
 mod utils;
 
+/// Handle homepage GET requests.
+/// 
+/// # Arguments
+/// 
+/// * `config` - Global application configuration.
 #[get("/")]
 fn index(config: State<AppConfig>) -> Template {
     let context = json!({
@@ -20,17 +31,24 @@ fn index(config: State<AppConfig>) -> Template {
     Template::render("home", &context)
 }
 
-#[get("/compare/<raw_pos>")]
-fn compare(config: State<AppConfig>, raw_pos: &rocket::http::RawStr) -> Template {
+/// Handle compare page GET requests.
+/// 
+/// # Arguments
+/// 
+/// * `config` - Global application configuration.
+/// * `raw_start_pos` - Raw file position from which search the closest line.
+#[get("/compare/<raw_start_pos>")]
+fn compare(config: State<AppConfig>, raw_start_pos: &rocket::http::RawStr) -> Template {
     let mut context = json!({
         "start_time": null,
         "pos": null,
         "next_pos": null,
-        "data": null
+        "data": null,
+        "ui_config": config.user_config.ui
     });
 
-    // parse position
-    let pos: u64 = match raw_pos.url_decode() {
+    // parse start position value
+    let start_pos: u64 = match raw_start_pos.url_decode() {
         Ok(s) => match s.parse() {
             Ok(v) => v,
             Err(e) => {
@@ -45,11 +63,11 @@ fn compare(config: State<AppConfig>, raw_pos: &rocket::http::RawStr) -> Template
     };
 
     // get data from file
-    let (data, next_pos) = match parse_line(&config.headers, &config.input, pos) {
+    let (data, pos, next_pos) = match parse_line(&config.headers, &config.input, start_pos) {
         Ok(v) => v,
         Err(e) => {
             println!("{}", e);
-            context["pos"] = Value::Number(serde_json::Number::from(pos));
+            context["pos"] = Value::Number(serde_json::Number::from(start_pos));
             return Template::render("errors/bad_parse", &context)
         }
     };
@@ -61,10 +79,17 @@ fn compare(config: State<AppConfig>, raw_pos: &rocket::http::RawStr) -> Template
     Template::render("qa/compare", &context)
 }
 
-#[post("/compare/<raw_pos>/apply", format = "json", data = "<raw_data>")]
-fn apply(config: State<AppConfig>, raw_pos: &rocket::http::RawStr, raw_data: Json<ApplyData>) -> &'static str {
+/// Handle compare match apply API endpoint POST requests.
+/// 
+/// # Arguments
+/// 
+/// * `config` - Global application configuration.
+/// * `raw_start_pos` - Raw file position from which search the closest line.
+/// * `raw_data` - Post match data as JSON.
+#[post("/compare/<raw_start_pos>/apply", format = "json", data = "<raw_data>")]
+fn apply(config: State<AppConfig>, raw_start_pos: &rocket::http::RawStr, raw_data: Json<ApplyData>) -> &'static str {
     // parse position
-    let pos: u64 = match raw_pos.url_decode() {
+    let start_pos: u64 = match raw_start_pos.url_decode() {
         Ok(s) => match s.parse() {
             Ok(v) => v,
             Err(e) => {
@@ -78,12 +103,15 @@ fn apply(config: State<AppConfig>, raw_pos: &rocket::http::RawStr, raw_data: Jso
         }
     };
     
+    // calculate match data and track time
     let data = raw_data.into_inner();
     let matched = if data.skip { "S" } else 
         if data.approved { "Y" } else { "N" };
     let track_time = (Utc::now().timestamp_nanos() - data.time) / 1000000;
 
-    if let Err(e) = write_line(&config, format!("{},{}", matched, track_time), pos, true) {
+    // join original line contents with match data and write to output file
+    let text = format!("{},{}", matched, track_time);
+    if let Err(e) = write_line(&config, text, start_pos, true) {
         println!("{}", e);
         return "Err";
     }
@@ -99,11 +127,15 @@ fn main() {
             (about: "Easily compare 2 products to approve or reject equality.")
             (@arg input_file: +required "Must provide an input CSV file path")
             (@arg output_file: +required "Must provide an output CSV file path")
+            (@arg config_file: +required format!(
+                "Must provide a JSON config file path, example:\n{}",
+                CONFIG_SAMPLE
+            ))
     ).get_matches();
 
     // build app config
     let input = clap.value_of("input_file").unwrap().to_string();
-    let (buf, start_pos) = match read_line(&input, 0) {
+    let (buf, start_pos, next_pos) = match read_line(&input, 0) {
         Ok(v) => v,
         Err(e) => {
             println!("Error reading headers from input file \"{}\": {}", &input, e);
@@ -117,11 +149,20 @@ fn main() {
             return;
         }
     };
+    let user_config_path = clap.value_of("config_file").unwrap().to_string();
+    let user_config = match UserConfig::from_file(user_config_path) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Error parsing config file \"{}\": {}", &user_config_path, e);
+            return;
+        }
+    };
     let config = AppConfig{
         input,
         output: clap.value_of("output_file").unwrap().to_string(),
         headers: headers,
-        start_pos
+        start_pos,
+        user_config
     };
 
     // write output headers
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,114 @@
+//! Utils module containing several functions and structs.
+
 use serde::{Serialize, Deserialize};
 use std::fs::{File, OpenOptions};
-use std::io::{self, Seek, SeekFrom, Write, BufRead};
+use std::io::{self, Seek, SeekFrom, Write, BufRead, BufWriter};
 
-#[derive(Debug)]
-pub struct AppConfig {
-    pub input: String,
-    pub output: String,
-    pub headers: String,
-    pub start_pos: u64
+const BUF_SIZE: usize = 4096;
+const EMPTY_RESULT_LINE_SIZE: usize = 40;
+
+/// User config sample file.
+const CONFIG_SAMPLE: &str = r#"
+{
+  "ui": {
+    "image_url": {
+      "a": "dh_image_url",
+      "b": "match_image_url",
+    },
+    "product_name": {
+      "a": "dh_product_name",
+      "b": "match_product_name",
+    },
+    "data": [
+      {
+        "label": "Size",
+        "a": "dh_size_std",
+        "b": "match_size_std"
+      }, {
+        "label": "Size Unit",
+        "a": "dh_size_unit",
+        "b": "match_size_unit"
+      }, {
+        "label": "Price",
+        "a": "dh_price",
+        "b": "match_price"
+      }, {
+        "label": "GID",
+        "a": "dh_global_id",
+        "b": null,
+        "show_more": true
+      }
+    ]
+  }
+}
+"#;
+
+/// UI data value used to describe an extra data compare field.
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct UiDataValue {
+    /// Label to be display on the compare UI.
+    pub label: String,
+    /// Product A field header key.
+    pub a: Option<String>,
+    /// Product B field header key.
+    pub b: Option<String>,
+    /// Show more flag, will be hidden when true until the user
+    /// enable `show more` feature.
+    pub show_more: Option<bool>
 }
 
+/// UI configuration used to describe the compare view.
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct UiConfig {
+    /// Image url compare UI configuration.
+    pub image_url: Option<UiDataValue>,
+    /// Product name compare UI configuration.
+    pub product_name: Option<UiDataValue>,
+    /// Extra data compare UI configuration collection.
+    pub data: Vec<UiDataValue>
+}
+
+/// User configuration build from a JSON file.
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct UserConfig {
+    /// UI configuration object.
+    pub ui: UiConfig
+}
+
+impl UserConfig {
+    /// Build a UiConfig object from a JSON file.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `path` - JSON file path.
+    pub fn from_file(path: &str) -> io::Result<UserConfig> {
+        // open the file in read-only mode with buffer
+        let file = File::open(path)?;
+        let reader = io::BufReader::new(file);
+    
+        // read the JSON contents of the file into the user config
+        let config = serde_json::from_reader(reader)?;
+        Ok(config)
+    }
+}
+
+/// Application configuration.
+#[derive(Debug)]
+pub struct AppConfig {
+    /// Input file path.
+    pub input: String,
+    /// Output file path.
+    pub output: String,
+    /// CSV input file headers line string.
+    pub headers: String,
+    /// First data line from the input CSV file.
+    pub start_pos: u64,
+    /// User configuration object created from the provided JSON
+    /// config file.
+    pub user_config: UserConfig
+}
+
+/// Data type describing an action to apply to the compare.
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 pub struct ApplyData {
     pub approved: bool,
@@ -17,47 +116,46 @@ pub struct ApplyData {
     pub time: i64
 }
 
-#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
-pub struct Record {
-    pub similarity: String,
-    #[serde(rename = "Match (Y/N)")]
-    pub match_y_n: String,
-    pub dh_product_name: String,
-    pub match_product_name: String,
-    pub dh_size_unit_std: String,
-    pub match_size_unit_std: String,
-    pub dh_size_std: String,
-    pub match_size_std: String,
-    pub dh_num_pieces: String,
-    pub match_num_pieces: String,
-    pub dh_price: String,
-    pub match_price: String,
-    pub dh_sku: String,
-    pub match_sku: String,
-    pub dh_img_url: String,
-    pub image_url: String,
-    #[serde(rename = "DH image")]
-    pub dh_image: String,
-    #[serde(rename = "Shopee image")]
-    pub shopee_image: String
-}
-
-pub fn read_line(path: &String, pos: u64) -> io::Result<(Vec<u8>, u64)> {
+/// Reads the closest line on a CSV file from the provided position
+/// and returns the line contents as bytes, the closes line position
+/// and the next line position.
+/// 
+/// # Arguments
+/// 
+/// * `path` - CSV file path.
+/// * `start_pos`  - File position from which search the closest line.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let file_path = "my_file.csv".to_string();
+/// let start_pos = 10;
+/// let (buf, pos, next_pos) = read_line(&file_path, start_pos).unwrap();
+/// println!(String::from_utf8(buf).unwrap());
+/// ```
+pub fn read_line(path: &String, start_pos: u64) -> io::Result<(Vec<u8>, u64, u64)> {
     let file = File::open(path)?;
     let mut reader = io::BufReader::new(file);
+    let mut pos = start_pos;
 
     // make sure the file pointer is at the start of a line
-    if pos > 0 {
-        reader.seek(SeekFrom::Start(pos-1))?;
+    if start_pos > 0 {
+        // find closest new line position
+        reader.seek(SeekFrom::Start(start_pos-1))?;
         let mut disposable_buf = Vec::new();
         reader.read_until(b'\n', &mut disposable_buf)?;
+
+        // move to closest line first char
         let current_pos = reader.stream_position()?;
-        reader.seek(SeekFrom::Start(current_pos + 1u64))?;
+        pos = current_pos + 1u64;
+        reader.seek(SeekFrom::Start(pos))?;
     }
 
     // read one line
     let mut buf = Vec::new();
     reader.read_until(b'\n', &mut buf)?;
+
+    // remove any new line at the end
     if let Some(last) = buf.last() {
         if *last == b'\n' {
             buf.pop();
@@ -69,48 +167,73 @@ pub fn read_line(path: &String, pos: u64) -> io::Result<(Vec<u8>, u64)> {
         }
     }
 
-    Ok((buf, reader.stream_position()?))
+    // return line bytes, line position and next line position
+    let next_pos = reader.stream_position()?;
+    Ok((buf, pos, next_pos))
 }
 
-pub fn parse_line(headers: &String, path: &String, pos: u64) -> Result<(Record, u64), String> {
-    let (raw_data, new_pos) = match read_line(path, pos) {
+/// Parse the closest line contents into a JSON object and returns
+/// the JSON object, the closest line position, and the next line
+/// position.
+/// 
+/// # Arguments
+/// 
+/// * `headers` - Headers line string.
+/// * `path` - CSV file path to read.
+/// * `start_pos` - File position from which search the closest line.
+pub fn parse_line(headers: &String, path: &String, start_pos: u64) -> Result<(serde_json::Value, u64, u64), String> {
+    // get closest line bytes, position and next line position
+    let (raw_data, pos, next_pos) = match read_line(path, start_pos) {
         Ok(v) => v,
         Err(e) => return Err(format!("{}", e))
     };
+
+    // build CSV string using headers and line bytes
     let csv_text = format!("{}\n{}", headers, String::from_utf8(raw_data).unwrap());
     
-    // read data from line by using the headers for easy access
+    // read data from the built CSV string by using the headers for easy access
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(true)
         .flexible(true)
         .from_reader(csv_text.as_bytes());
     
+    // deserialize CSV string object into a JSON object
     for result in rdr.deserialize() {
         match result {
-            Ok(raw_record) => {
-                let record: Record = raw_record;
-                return Ok((record, new_pos))
+            Ok(record) => {
+                // return data after the first successful record
+                return Ok((record, pos, next_pos))
             }
             Err(e) => {
-                println!("Couldn't parse the data at position {}: {}", pos, e);
+                println!("Couldn't parse the data at position {}: {}", start_pos, e);
             }
         }
     }
-    Err(format!("Couldn't parse the data at position {}", pos))
+
+    // error out if no valid record found
+    Err(format!("Couldn't parse the data at position {}", start_pos))
 }
 
-pub fn write_line(config: &AppConfig, text: String, pos: u64, append: bool) -> io::Result<()> {
+/// Write match data to the output file by using the closes line
+/// data from the input file. Return io::Result.
+/// 
+/// # Arguments
+/// 
+/// * `config` - Application configuration containing input, output and headers data.
+/// * `start_pos` - File position from which search the closest line.
+/// * `append` - Append flag to decide whenever append or override the output file.
+pub fn write_line(config: &AppConfig, text: String, start_pos: u64, append: bool) -> io::Result<()> {
     // get data from input file
-    let (buf, _) = read_line(&config.input, pos)?;
+    let (buf, pos, _) = read_line(&config.input, start_pos)?;
 
-    // decide on append or just override
+    // decide on append or just override, then open file
     let mut output_file = if append {
         OpenOptions::new().create(true).append(true).open(&config.output)?
     } else {
         OpenOptions::new().create(true).write(true).truncate(true).open(&config.output)?
     };
 
-    // open file and write
+    // write new match data to output file
     let text = match text.len() {
         0 => format!("{}{}", String::from_utf8(buf).unwrap(), text),
         _ => format!("{},{}", String::from_utf8(buf).unwrap(), text)
@@ -118,4 +241,65 @@ pub fn write_line(config: &AppConfig, text: String, pos: u64, append: bool) -> i
     writeln!(output_file, "{}", text)?;
 
     Ok(())
+}
+
+/// Analyze an input file to track new lines and record it's positions.
+/// Returns total line count.
+/// 
+/// # Arguments
+/// 
+/// * `input_path` - File path to analize.
+/// * `result_path` - File path to write results.
+pub fn analize(input_path: &str, result_path: &str) -> io::Result<u64> {
+    let file = File::open(input_path)?;
+    let result_file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(result_path)?;
+    
+    let empty_line: [U8; EMPTY_RESULT_LINE_SIZE] = [u8; EMPTY_RESULT_LINE_SIZE];
+    file.write
+
+    unimplemented!();
+}
+
+/// Fill a file with zero byte until the target size or ignore if
+/// bigger. Return true if file is bigger.
+/// 
+/// # Arguments
+/// 
+/// * `path` - File path to fill.
+/// * `target_size` - Target file size in bytes.
+pub fn fill_file(path: &str, target_size: u64) -> io::Result<bool> {
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(path)?;
+
+    file.sync_all()?;
+    let size = file.metadata()?.len();
+
+    // validate file current size vs target size
+    if target_size < size {
+        // file is bigger, return true
+        return Ok(true);
+    }
+    if target_size == size {
+        return Ok(false);
+    }
+
+    // fill file with zeros until target size is match
+    let buf_size = 4096u64;
+    let buf = [0u8; 4096];
+    let wrt = BufWriter::new(file);
+    while size + buf_size < target_size {
+        wrt.write_all(&buf)?;
+        size += buf_size;
+    }
+    let remaining = (target_size - size) as usize;
+    if remaining > 0 {
+        wrt.write_all(&buf[..remaining])?;
+    }
+
+    Ok(false)
 }
